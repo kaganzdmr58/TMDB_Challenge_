4f491abf56e13e8e36a7da90bb6c6bb1ff311adc
kagan_ozdemir <kaganzdmr58@gmail.com>
2022-06-27 14:54:08 +0300

    Revert "TMDB_CHALLANGE"
    This reverts commit 03a06aa703dbe4c9518b361dad96a77ae5abe305.

--- app/src/main/java/com/cbagames/tmdb_challenge/Videos.kt
+++
@@ -1,6 +0,0 @@
-package com.cbagames.tmdb_challenge
-
-import java.io.Serializable
-
-data class Videos (val id : Int, val name:String, val poster_path:String, val media_type:String):Serializable {
-}
